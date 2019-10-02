10dcc0d78b6b2b69854ea5c23bd6832c335ed441
kymkcj23 <kymkcj23@naver.com>
2019-10-03 00:07:40 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse('여기는 홈 페이지')
