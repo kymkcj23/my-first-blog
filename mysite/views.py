e30237a722acf3cb3ffbfe355c90a47a54a5f013
7497 <kymkcj23@daum.net>
2019-10-03 01:16:38 +0900

    Changed the HTML for the site.

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,6 @@
-```python {.line-numbers}
-  # mysite/views.py
-  from django.shortcuts import render
-  from django.http import HttpResponse
 
-  def homepage(request):
-      return HttpResponse('여기는 홈 페이지')
-  ```
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse('여기는 홈 페이지')
