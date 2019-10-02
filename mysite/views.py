d9f5bc5776d6783ce5c494c3dcd5a75f74735972
7497 <kymkcj23@daum.net>
2019-10-03 01:15:19 +0900

    first commit

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+```python {.line-numbers}
+  # mysite/views.py
+  from django.shortcuts import render
+  from django.http import HttpResponse
 
-def homepage(request):
-    return HttpResponse('여기는 홈 페이지')
+  def homepage(request):
+      return HttpResponse('여기는 홈 페이지')
+  ```
