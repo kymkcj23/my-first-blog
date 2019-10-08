fe41d8aae30d45eb6c4f7f1f839a8762b28d0d07
kymkcj23 <kymkcj23@naver.com>
2019-10-08 11:26:38 +0900

    정적 컨텐츠

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
-
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def homepage(request):
-    return HttpResponse('여기는 정아음의 홈 페이지')
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
