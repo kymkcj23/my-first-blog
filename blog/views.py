cff49fa4f7bb8ecc73c92538797301abd783123d
kymkcj23 <kymkcj23@naver.com>
2019-10-08 11:58:26 +0900

    장고컨텐츠

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
-=======
-여기는 홈 페이지
->>>>>>> 043bc4275c5f46efcaa1c07f3eff47c942be1a9e
+
